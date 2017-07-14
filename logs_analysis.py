74fd7d3cf29b73c11aa4c4a0fa78bdd6bf7b26fa
zoleitschuk <zachary.oleitschuk@gmail.com>
2017-07-14 11:36:29 -0600

    changes to pass pep8

--- logs_analysis.py
+++ logs_analysis.py
@@ -6,70 +6,96 @@ Created on Thu Jul 13 11:50:05 2017
 """
 import psycopg2
 
+
 def get_top_articles():
     # Connect to the news db and create a cursor object
     conn = psycopg2.connect('dbname=news')
     cursor = conn.cursor()
-    
+
     query_string = """
-        SELECT articles.title, path_views.views FROM
-        articles JOIN 
-            (SELECT replace(path,'-',' ') as title, count(*) as views FROM log
-                WHERE method='GET' AND status='200 OK' AND path!='/'
+        SELECT articles.title, path_views.views
+        FROM articles
+        JOIN
+            (SELECT replace(path,'-',' ') as title, count(*) as views
+                FROM log
+                WHERE method='GET'
+                    AND status='200 OK'
+                    AND path!='/'
                 GROUP BY path
-                ORDER BY views DESC) as path_views
-        ON replace(lower(replace(articles.title, '''', '')), 'there are a lot of', 'so many') LIKE
-            concat('%', replace(path_views.title, '/article/', ''), '%')
+                ORDER BY views DESC
+            ) AS path_views
+        ON replace(
+                lower(replace(articles.title, '''', '')),
+                   'there are a lot of',
+                   'so many')
+            LIKE concat(
+                '%',
+                replace(path_views.title, '/article/', ''),
+                '%')
         ORDER BY path_views.views DESC
         LIMIT 3;
         """
     cursor.execute(query_string)
     rows = cursor.fetchall()
-    
+
     # Print results to the command line
     for row in rows:
         print('"' + row[0] + '"' + ' - ' + str(row[1]) + ' views')
-    
+
     # Close the cursor and connection
     cursor.close()
     conn.close()
+
 
 def get_authors_by_views():
     # Connect to the news db and create a cursor object
     conn = psycopg2.connect('dbname=news')
     cursor = conn.cursor()
-    
+
     query_string = """
-        SELECT authors.name, sum(article_views.views) as author_views FROM
-        authors JOIN
-            (SELECT * FROM
-            articles JOIN 
-                (SELECT replace(path,'-',' ') as title, count(*) as views FROM log
-                    WHERE method='GET' AND status='200 OK' AND path!='/'
-                    GROUP BY path
-                    ORDER BY views DESC) as path_views
-            ON replace(lower(replace(articles.title, '''', '')), 'there are a lot of', 'so many') LIKE
-                concat('%', replace(path_views.title, '/article/', ''), '%')) as article_views
+        SELECT authors.name, sum(article_views.views) as author_views
+        FROM authors
+        JOIN
+            (SELECT * FROM articles
+                JOIN
+                    (SELECT replace(path,'-',' ') as title, count(*) as views
+                        FROM log
+                        WHERE method='GET'
+                            AND status='200 OK'
+                            AND path!='/'
+                        GROUP BY path
+                        ORDER BY views DESC
+                    ) AS path_views
+                ON replace(
+                        lower(replace(articles.title, '''', '')),
+                        'there are a lot of',
+                        'so many')
+                    LIKE concat(
+                        '%',
+                        replace(path_views.title, '/article/', ''),
+                        '%')
+            ) AS article_views
         ON authors.id=article_views.author
         GROUP BY authors.id
         ORDER BY author_views DESC;
         """
-        
+
     cursor.execute(query_string)
     rows = cursor.fetchall()
-    
+
     # Print results to the command line
     for row in rows:
         print(row[0] + ' - ' + str(row[1]))
-    
+
     # Close the cursor and connection
     cursor.close()
     conn.close()
 
+
 def get_error_log():
     conn = psycopg2.connect('dbname=news')
     cursor = conn.cursor()
-    
+
     query_string = """
         SELECT a.date, (CAST(a.requests AS FLOAT)*100/(a.requests+b.requests))
             FROM
@@ -85,17 +111,18 @@ def get_error_log():
                 AND (CAST(a.requests AS FLOAT)*100/(a.requests+b.requests))>1
             ORDER BY a.date DESC;
     """
-    
+
     cursor.execute(query_string)
     rows = cursor.fetchall()
-    
+
     # Print results to the command line
     for row in rows:
         print(str(row[0]) + ' - ' + str(round(row[1], 1)) + '% errors')
-    
+
     # Close the cursor and connection
     cursor.close()
     conn.close()
+
 
 if __name__ == '__main__':
     print('Top 3 artciles by number of views:')
