41d4101ea92d3073ef342c24d1fa7dc4afd3a279
zoleitschuk <zachary.oleitschuk@gmail.com>
2017-07-16 14:22:05 -0600

    assorted refactoring as per code review

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 A number of functions for printing a number of useful metrics on a local news
@@ -9,6 +9,32 @@ Created on Thu Jul 13 11:50:05 2017
 @author: Zachary Oleitschuk
 """
 import psycopg2
+
+
+def execute_query(query):
+    """Executes a query on the  news database and returns resutls.
+
+    Connects to and queries the local news db based on the passed query arg.
+    Then closes the connection to the database and returns the result of the
+    query.
+
+    Args:
+        query - string containing sql to be executed
+
+    Returns:
+        results - the results of executing the input query string
+    """
+    conn = psycopg2.connect('dbname=news')
+    cursor = conn.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    return results
 
 
 def get_top_articles():
@@ -25,47 +51,32 @@ def get_top_articles():
     Returns:
         n/a
     """
-    # Connect to the news db and create a cursor object
-    conn = psycopg2.connect('dbname=news')
-    cursor = conn.cursor()
 
     # We query the news db in order to return an ordered table of articles and
-    # article view pairs. By joining the log and articles tables on modified
-    # log.path and articles.title columns we get a table of total article
+    # article view pairs. By joining the log and articles tables on
+    # log.path and articles.slug columns we get a table of total article
     # views.
-    query_string = """
+    query = """
         SELECT articles.title, path_views.views
         FROM articles
         JOIN
-            (SELECT replace(path,'-',' ') as title, count(*) as views
+            (SELECT path, count(*) as views
                 FROM log
                 WHERE method='GET'
                     AND status='200 OK'
                     AND path!='/'
                 GROUP BY path
-                ORDER BY views DESC
             ) AS path_views
-        ON replace(
-                lower(replace(articles.title, '''', '')),
-                   'there are a lot of',
-                   'so many')
-            LIKE concat(
-                '%',
-                replace(path_views.title, '/article/', ''),
-                '%')
+        ON path_views.path LIKE concat('%', articles.slug, '%')
         ORDER BY path_views.views DESC
         LIMIT 3;
         """
-    cursor.execute(query_string)
-    rows = cursor.fetchall()
+
+    results = execute_query(query)
 
     # Print results to the command line
-    for row in rows:
-        print('"' + row[0] + '"' + ' - ' + str(row[1]) + ' views')
-
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    for title, views in results:
+        print('"{}" - {} views'.format(title, views))
 
 
 def get_authors_by_views():
@@ -83,9 +94,6 @@ def get_authors_by_views():
     Returns:
         n/a
     """
-    # Connect to the news db and create a cursor object
-    conn = psycopg2.connect('dbname=news')
-    cursor = conn.cursor()
 
     # We query the news db in order to return an ordered table of author and
     # article view pairs. By joining the log and articles tables on modified
@@ -93,43 +101,31 @@ def get_authors_by_views():
     # We can then join that table with the authors table on the FK relationship
     # and group by author while summing on article views to get a table of
     # authors and their total article views.
-    query_string = """
+    query = """
         SELECT authors.name, sum(article_views.views) as author_views
         FROM authors
         JOIN
             (SELECT * FROM articles
                 JOIN
-                    (SELECT replace(path,'-',' ') as title, count(*) as views
+                    (SELECT path, count(*) as views
                         FROM log
                         WHERE method='GET'
                             AND status='200 OK'
                             AND path!='/'
                         GROUP BY path
-                        ORDER BY views DESC
                     ) AS path_views
-                ON replace(
-                        lower(replace(articles.title, '''', '')),
-                        'there are a lot of',
-                        'so many')
-                    LIKE concat(
-                        '%',
-                        replace(path_views.title, '/article/', ''),
-                        '%')
+                ON path_views.path LIKE  concat('%', articles.slug, '%')
             ) AS article_views
         ON authors.id=article_views.author
         GROUP BY authors.id
         ORDER BY author_views DESC;
         """
-    cursor.execute(query_string)
-    rows = cursor.fetchall()
+
+    results = execute_query(query)
 
     # Print results to the command line
-    for row in rows:
-        print(row[0] + ' - ' + str(row[1]))
-
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    for author, views in results:
+        print('{} - {}'.format(author, views))
 
 
 def get_error_log():
@@ -146,49 +142,46 @@ def get_error_log():
     Returns:
         n/a
     """
-    # Connect to the news db and create a cursor object
-    conn = psycopg2.connect('dbname=news')
-    cursor = conn.cursor()
 
     # We query the db to get pairs of dates and error percentages where
     # the error percentage is greater than 1% by performing a filtered self-
     # join on the log table. The self-join results in a.requests containing
     # a count of the failed requests and b.requests containing a count of
     # the successful requests.
-    query_string = """
-        SELECT a.date, (CAST(a.requests AS FLOAT)*100/(a.requests+b.requests))
+    query = """
+        SELECT a.date, (CAST(a.requests AS FLOAT)*100/(b.requests))
             FROM
-                (SELECT date(time) AS date, status, count(*) AS requests
+                (SELECT date(time) AS date, count(*) AS requests
                     FROM log
-                    GROUP BY date, status) AS a,
-                (SELECT date(time) AS date, status, count(*) AS requests
+                    WHERE status!='200 OK'
+                    GROUP BY date) AS a,
+                (SELECT date(time) AS date, count(*) AS requests
                     FROM log
-                    GROUP BY date, status) AS b
+                    GROUP BY date) AS b
             WHERE a.date=b.date
-                AND a.status!='200 OK'
-                AND a.status!=b.status
-                AND (CAST(a.requests AS FLOAT)*100/(a.requests+b.requests))>1
+                AND (CAST(a.requests AS FLOAT)*100/(b.requests))>1
             ORDER BY a.date DESC;
     """
-    cursor.execute(query_string)
-    rows = cursor.fetchall()
+
+    results = execute_query(query)
 
     # Print results to the command line
-    for row in rows:
-        print(str(row[0]) + ' - ' + str(round(row[1], 1)) + '% errors')
-
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    for date, err_percent in results:
+        print('{:%B %d, %Y} - {:.1f}% errors'.format(date, err_percent))
 
 
 if __name__ == '__main__':
-    print('Top 3 artciles by number of views:')
-    print('----------------------------------')
+    title = 'Top 3 artciles by number of views:'
+    print(title)
+    print('-'*len(title))
     get_top_articles()
-    print('\nAuthor Ranking by total article views:')
-    print('--------------------------------------')
+
+    title = '\nAuthor Ranking by total article views:'
+    print(title)
+    print('-'*len(title))
     get_authors_by_views()
-    print('\nDays where more than 1% of requests resulted in an error:')
-    print('-----------------------------------------------------------')
+
+    title = '\nDays where more than 1% of requests resulted in an error:'
+    print(title)
+    print('-'*len(title))
     get_error_log()
